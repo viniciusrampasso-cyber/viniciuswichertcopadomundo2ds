d08a94610eb4844833fd9a47bbe7808fe509b6d9
Vinicius Wichert <vinicius.rampasso@escola.pr.gov.br>
2026-06-19 11:19:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function mostrarCopa(ano, botao){
 
+    document.getElementById("titulo").innerText =
+        "Copa do Mundo " + ano;
+
+    document.getElementById("texto").innerText =
+        copas[ano];
+
+    document
+        .querySelectorAll(".sidebar button")
+        .forEach(btn => btn.classList.remove("selecionado"));
+
+    botao.classList.add("selecionado");
+
+    const content = document.querySelector(".content");
+
+    content.classList.remove("animar");
+
+    void content.offsetWidth;
+
+    content.classList.add("animar");
+}
